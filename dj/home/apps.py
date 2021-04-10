76325074a625b971f3aca449e5b6e1a2c3369835
Takayuki Shimizukawa <takayuki.shimizukawa@beproud.jp>
2021-04-10 16:54:22 +0900

    fix nits

--- dj/home/apps.py
+++ dj/home/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class HomeConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'dj.home'
+    name = 'home'
--- dj/otel/urls.py
+++ dj/otel/urls.py
@@ -14,8 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from dj.home import views as home_views
+from django.urls import path, include
+from home import views as home_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),

59a6f8a181169a4ac87b5db2f1a26b4b6e7c9c9d
shimizukawa <shimizukawa@gmail.com>
2022-09-05 20:40:06 +0000

    attach console logging to span

--- console/log.py
+++ console/log.py
@@ -2,15 +2,39 @@
 setup logging
 """
 
+import logging
 from logging.config import dictConfig
 
 from opentelemetry.sdk.resources import Resource
-from opentelemetry.sdk._logs import LogEmitterProvider, set_log_emitter_provider
+from opentelemetry.sdk._logs import LogEmitterProvider, set_log_emitter_provider, LoggingHandler
 from opentelemetry.sdk._logs.export import BatchLogProcessor, SimpleLogProcessor, ConsoleLogExporter
+from opentelemetry.sdk._logs.severity import std_to_otlp
 from opentelemetry.exporter.otlp.proto.grpc._log_exporter import OTLPLogExporter
+from opentelemetry.trace.propagation import get_current_span
+
+
+class SpanLoggingHandler(LoggingHandler):
+
+    def emit(self, record: logging.LogRecord) -> None:
+        translated_record = self._translate(record)
+        attributes = {
+            key: value
+            for (key, value) in translated_record.attributes.items()
+            if not key.startswith('otel')
+        } | dict(
+            severity_text = record.levelname,
+            severity_number = std_to_otlp(record.levelno).value,
+        )
+        span = get_current_span()
+        span.add_event(
+            translated_record.body,
+            attributes,
+            translated_record.timestamp
+        )
 
 
 def setup(resource: Resource):
+    # setup log exporter
     # from https://github.com/open-telemetry/opentelemetry-python/blob/69c9e39/docs/examples/logs/example.py
     log_emitter_provider = LogEmitterProvider(resource=resource)
     set_log_emitter_provider(log_emitter_provider)
@@ -37,12 +61,15 @@ def setup(resource: Resource):
                 'class': 'logging.StreamHandler',
                 'formatter': 'standard'
             },
-            'otel': {  # Attach OTLP handler to root logger
+            'otel_log': {  # Attach OTLP log handler to root logger
                 '()': 'opentelemetry.sdk._logs.LoggingHandler',
-            }
+            },
+            'otel_span': {  # Attach OTLP span handler to root logger
+                '()': SpanLoggingHandler,
+            },
         },
         'root': {  # Catch all
-            'handlers': ['console', 'otel'],
+            'handlers': ['console', 'otel_log', 'otel_span'],
             'level': 'NOTSET',
         },
     }

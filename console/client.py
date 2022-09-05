6041d26b51abd499a8c21727ef88bec800c606a0
shimizukawa <shimizukawa@gmail.com>
2022-09-05 11:58:25 +0000

    add logger console/client

--- console/client.py
+++ console/client.py
@@ -1,3 +1,5 @@
+# https://github.com/open-telemetry/opentelemetry-python/blob/e1a4c38/docs/examples/django/client.py
+
 import requests
 
 from opentelemetry import trace
@@ -18,17 +20,28 @@ trace.get_tracer_provider().add_span_processor(
     SimpleSpanProcessor(ConsoleSpanExporter())
 )
 
-# otelp exporter
+# otelp span exporter
 from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
 trace.get_tracer_provider().add_span_processor(
     BatchSpanProcessor(OTLPSpanExporter(endpoint="localhost:4317", insecure=True))
 )
 
+# from https://github.com/open-telemetry/opentelemetry-python/blob/69c9e39/docs/examples/logs/example.py
+from opentelemetry.sdk._logs import LogEmitterProvider, set_log_emitter_provider
+from opentelemetry.sdk._logs.export import BatchLogProcessor
+from opentelemetry.exporter.otlp.proto.grpc._log_exporter import OTLPLogExporter
+log_emitter_provider = LogEmitterProvider(resource=resource)
+set_log_emitter_provider(log_emitter_provider)
+log_emitter_provider.add_log_processor(
+    BatchLogProcessor(OTLPLogExporter(endpoint="localhost:4317", insecure=True))
+)
+
+# add handler in logging_config
+
 # setup logging
 from logging.config import dictConfig
-
-config = {
+logging_config = {
     'version': 1,
     'disable_existing_loggers': False,
     'formatters': {
@@ -42,13 +55,20 @@ config = {
             'class': 'logging.StreamHandler',
             'formatter': 'standard'
         },
+        'otel': {  # Attach OTLP handler to root logger
+            '()': 'opentelemetry.sdk._logs.LoggingHandler',
+        }
     },
-    'root': {  # 全てキャッチする
-        'handlers': ['console'],
+    'root': {  # Catch all
+        'handlers': ['console', 'otel'],
         'level': 'NOTSET',
     },
 }
-dictConfig(config)
+dictConfig(logging_config)
+import logging
+logger = logging.getLogger(__name__)
+
+tracer = trace.get_tracer_provider().get_tracer(__name__)
 
 
 def main():
@@ -56,14 +76,26 @@ def main():
     import opentelemetry.instrumentation.requests
     opentelemetry.instrumentation.requests.RequestsInstrumentor().instrument()
 
-    # Logging（ログ出力へのtrace_id等差し込み）
+    # Logging (inject trace_id etc into logger)
     from opentelemetry.instrumentation.logging import LoggingInstrumentor
     LoggingInstrumentor().instrument()
 
-    # action
-    response = requests.get('http://localhost:8000/')
-    assert response.status_code == 200
+    logger.info('Hello')
+
+    with tracer.start_as_current_span("requests"):
+        # inject will do in otel framework.
+        # from opentelemetry.propagate import inject
+        # headers = {}; inject(headers)
+
+        # action
+        response = requests.get(
+            'http://localhost:8000/',
+            params={'param': 1234},
+        )
+        assert response.status_code == 200
 
 
 if __name__ == '__main__':
-    main()
+    with tracer.start_as_current_span("client"):
+        main()
+    log_emitter_provider.shutdown()

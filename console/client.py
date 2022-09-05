5eb8d220ea94e65baf533914c0ef4ae8e8d6d396
shimizukawa <shimizukawa@gmail.com>
2022-09-05 10:49:47 +0000

    add console/client logging

--- console/client.py
+++ console/client.py
@@ -25,11 +25,40 @@ trace.get_tracer_provider().add_span_processor(
     BatchSpanProcessor(OTLPSpanExporter(endpoint="localhost:4317", insecure=True))
 )
 
+# setup logging
+from logging.config import dictConfig
+
+config = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'standard': {
+            'format': ('%(levelname)s [%(asctime)s] [trace_id=%(otelTraceID)s span_id=%(otelSpanID)s resource.service.name=%(otelServiceName)s] %(name)s %(message)s'),
+        },
+    },
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'standard'
+        },
+    },
+    'root': {  # 全てキャッチする
+        'handlers': ['console'],
+        'level': 'NOTSET',
+    },
+}
+dictConfig(config)
+
 
 def main():
-    # instrumentation
+    # requests instrumentation
     import opentelemetry.instrumentation.requests
     opentelemetry.instrumentation.requests.RequestsInstrumentor().instrument()
+
+    # Logging（ログ出力へのtrace_id等差し込み）
+    from opentelemetry.instrumentation.logging import LoggingInstrumentor
+    LoggingInstrumentor().instrument()
 
     # action
     response = requests.get('http://localhost:8000/')

e947ef352dd2a344118452465d616d042475b7d3
shimizukawa <shimizukawa@gmail.com>
2022-11-05 03:21:30 +0000

    update opentelemetry python packages https://github.com/open-telemetry/opentelemetry-python/commit/8318f0121733cabf9e69743d07213e65cc300d88

--- backend/config/log.py
+++ backend/config/log.py
@@ -5,8 +5,8 @@ setup logging
 import logging
 
 from opentelemetry.sdk.resources import Resource
-from opentelemetry.sdk._logs import LogEmitterProvider, set_log_emitter_provider, LoggingHandler
-from opentelemetry.sdk._logs.export import BatchLogProcessor, SimpleLogProcessor, ConsoleLogExporter
+from opentelemetry.sdk._logs import LoggerProvider, set_logger_provider, LoggingHandler
+from opentelemetry.sdk._logs.export import BatchLogRecordProcessor, SimpleLogRecordProcessor, ConsoleLogExporter
 from opentelemetry.sdk._logs.severity import std_to_otlp
 from opentelemetry.exporter.otlp.proto.grpc._log_exporter import OTLPLogExporter
 from opentelemetry.trace.propagation import get_current_span
@@ -39,12 +39,12 @@ def setup(resource: Resource|None = None):
 
     # setup log exporter
     # from https://github.com/open-telemetry/opentelemetry-python/blob/69c9e39/docs/examples/logs/example.py
-    log_emitter_provider = LogEmitterProvider(resource=resource)
-    set_log_emitter_provider(log_emitter_provider)
-    log_emitter_provider.add_log_processor(
-        BatchLogProcessor(OTLPLogExporter(endpoint="localhost:4317", insecure=True))
+    log_emitter_provider = LoggerProvider(resource=resource)
+    set_logger_provider(log_emitter_provider)
+    log_emitter_provider.add_log_record_processor(
+        BatchLogRecordProcessor(OTLPLogExporter(endpoint="localhost:4317", insecure=True))
     )
-    log_emitter_provider.add_log_processor(
-        SimpleLogProcessor(ConsoleLogExporter())
+    log_emitter_provider.add_log_record_processor(
+        SimpleLogRecordProcessor(ConsoleLogExporter())
     )
     # add handler in settings.LOGGING
